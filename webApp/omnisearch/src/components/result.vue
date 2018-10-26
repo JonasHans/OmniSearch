ac2882ef329d2c090cf95b077caf143a1a06a08e
JonasHans <jonas.vanoenen@gmail.com>
2018-10-26 16:29:42 +0200

    Wordcloud Gods have blessed us

--- webApp/omnisearch/src/components/result.vue
+++ webApp/omnisearch/src/components/result.vue
@@ -40,29 +40,33 @@
 		},
 		computed : {
 			words: function() {
+				let stopWords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", 'now']
 				let cloud = []
 				let text = this.result._source.entire_text.split(" ")
 				let found = false
 
 				for (let t in text) {
-					for (let pair in cloud){
-						if (cloud[pair].name == text[t]) {
-							cloud[pair].value += 1
-							found = true
-						}
-					}
 
-					if(!found) {
-						cloud.push({
-							'name': text[t],
-							'value': 1
-						})
-					}else {
-						found = false
+					if (!stopWords.includes(text[t].toLowerCase())){
+						for (let pair in cloud){
+							if (cloud[pair].name == text[t]) {
+								cloud[pair].value += 1
+								found = true
+							}
+						}
+
+						if(!found) {
+							cloud.push({
+								'name': text[t],
+								'value': 1
+							})
+						}else {
+							found = false
+						}
 					}
 				}
 
-				return cloud
+				return cloud.slice(0,15)
 			}
 		},
 		components : {
